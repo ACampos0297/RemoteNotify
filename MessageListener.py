30a6ec0bc8f3bc115c7217d12e4d485f69944610
Abdul Campos <abdulcampos02@gmail.com>
2020-05-26 12:09:25 -0500

    Changed location of api keys and implemented config parser to fetch them

--- MessageListener.py
+++ MessageListener.py
@@ -3,9 +3,18 @@
 #from ScreenNotification import ScreenNotification
 import time
 import tweepy
-import json
+import configparser
 
 def main():
+    config = configparser.ConfigParser()
+    config.read('settings.ini')
+
+    consumer_key = config["keys"]["consumer_key"]
+    consumer_secret = config["keys"]["consumer_secret"]
+
+    access_token = config["keys"]["access_token"]
+    access_token_secret = config["keys"]["access_token_secret"]
+
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
@@ -15,8 +24,6 @@ def main():
 
     for tweet in timeline:
         print(tweet.text)
-
-
 
 if __name__ == "__main__":
     main()
